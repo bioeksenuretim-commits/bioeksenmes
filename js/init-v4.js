18c2460cd6ed31cca16112d7aff7d44610c3a551
Erenardal <alierenardal.mbg@gmail.com>
2026-06-05 11:11:41 +0300

    Bump app build version for refresh notice

--- js/init-v4.js
+++ js/init-v4.js
@@ -1,4 +1,4 @@
-const APP_BUILD_VERSION = '20260604-dev-final-stock-aggregate';
+const APP_BUILD_VERSION = '20260605-sales-lines-env-guard';
 window.APP_BUILD_VERSION = APP_BUILD_VERSION;
 console.log('APP_BUILD v4 aktif', APP_BUILD_VERSION);
 /**
